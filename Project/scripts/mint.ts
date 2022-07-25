a5dacc6012ff085eeceaa43c36e81261005c95ad
Yunus YILDIRIM <y.yildirim.ce@gmail.com>
2022-07-25 14:49:11 +0300

    Fix mint transaction

--- Project/scripts/mint.ts
+++ Project/scripts/mint.ts
@@ -1,36 +1,36 @@
 import { ethers } from "ethers";
 import "dotenv/config";
-import * as tokenJson from "artifacts/contracts/MyToken.sol/MyToken.json"
-import { MyToken } from "typechain";
 
-// This key is already public on Herong's Tutorial Examples - v1.03, by Dr. Herong Yang
-// Do never expose your keys like this
-const EXPOSED_KEY = "8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
+import * as tokenJson from "../artifacts/contracts/Token.sol/MyToken.json";
+import { getSigner } from "./utils";
+import { MyToken } from "../typechain/MyToken";
 
-function setupProvider() {
-    const alchemy = process.env.ALCHEMY_API_KEY;
-    const provider = ethers.providers.getDefaultProvider("goerli", alchemy);
-    return provider;
-  }
+async function main() {
+  const signer = getSigner();
 
-  async function main() {
-    const wallet =
-      process.env.MNEMONIC && process.env.MNEMONIC.length > 0
-        ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
-        : new ethers.Wallet(process.env.PRIVATE_KEY ?? EXPOSED_KEY);
-    console.log(`Using address ${wallet.address}`);
-  
-    const provider = setupProvider();
-    const signer = wallet.connect(provider);
-    const tokenFactory = new ethers.ContractFactory(
-        tokenJson.abi,
-        tokenJson.bytecode,
-        signer
-      );
-    const tokenContract = await tokenFactory.attach(process.env.TOKEN_ADDRESS) as MyToken;
-    const mintTx = await tokenContract.mint(
-      );
+  const tokenFactory = new ethers.ContractFactory(
+    tokenJson.abi,
+    tokenJson.bytecode,
+    signer
+  );
 
+  const tokenContract = (await tokenFactory.attach(
+    process.env.TOKEN_ADDRESS!
+  )) as MyToken;
+
+  console.log(`Token Contract address: ${tokenContract.address}`);
+
+  const voterAddress = process.argv[2];
+  const amount = process.argv[3];
+
+  console.log(`Minting ${amount} tokens to ${voterAddress} address`);
+  const mintTx = await tokenContract.mint(
+    voterAddress,
+    ethers.utils.parseEther(amount.toString())
+  );
+  console.log("Awaiting confirmations");
+  mintTx.wait;
+  console.log(`Transaction completed. Hash: ${mintTx.hash}`);
 }
 
 main().catch((error) => {
